7a52585fd077c082b5a79d4ec2778f97dd81c15b
Bhavesh Chavhan <97518972+Bhavesh299@users.noreply.github.com>
2022-11-02 16:52:22 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Forecast from "./components/Fore_Cast";
 import Statistics from "./components/Statistics";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   let [loading, setLoading] = useState(true);
@@ -103,7 +104,9 @@ setTimeout(() =>{
 
   return (
     <Box className="App" maxW="40rem" margin="0 auto" p='1rem 0' position="relative">
+     
       <Box className="searchBox" p="1rem">
+      <Navbar/>
         <SearchBar
           currentLocation={currentLocation}
           currentLocationFind={currentLocationFind}
